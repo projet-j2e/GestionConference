b2d9cc3798eafffbc4abba2c70dafb28e6dd1762
hamza-hd <hamza16haddad@gmail.com>
2019-02-02 15:53:12 +0100

    projet vide

---
+++ src/main/java/com/mql/GestionConferenceApplication.java
@@ -0,0 +1,14 @@
+package com.mql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionConferenceApplication.class, args);
+	}
+
+}
+
